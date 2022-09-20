62fdaa372207164ff0cc061e980135e2d48738c0
Veles-Pan <barbic-vika@ya.ru>
2022-09-20 23:30:18 +0100

    feat: add react-router

---
+++ src/pages/CounterPage/CounterPage.tsx
@@ -0,0 +1,11 @@
+import { Counter } from '../../components/Counter'
+
+const CounterPage = () => {
+  return (
+    <div className="counter">
+      <p>This is counter page</p> <Counter />
+    </div>
+  )
+}
+
+export default CounterPage
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import { Counter } from '../../components/Counter'
+
+const MainPage = () => {
+  return (
+    <div className="main">
+      <p>This is Main Page</p>
+    </div>
+  )
+}
+
+export default MainPage
