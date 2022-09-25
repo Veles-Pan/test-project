b5d8fd2c8fb837b049616a101157433e28474ef7
Veles-Pan <barbic-vika@ya.ru>
2022-09-25 18:59:29 +0100

    feat: add jest test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,26 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('test with only one param', () => {
+    expect(classNames('className')).toBe('className');
+  });
+
+  test('test with additional params', () => {
+    expect(classNames('className', {}, ['classNameAddition1', 'classNameAddition2'])).toBe('className classNameAddition1 classNameAddition2');
+  });
+
+  test('test with mods params', () => {
+    expect(classNames('className', { classNameTrue: true, classNameFalse: false }, ['classNameAddition1', 'classNameAddition2']))
+      .toBe('className classNameAddition1 classNameAddition2 classNameTrue');
+  });
+
+  test('test with only mods params', () => {
+    expect(classNames('', { classNameTrue: true, classNameFalse: false }, ['']))
+      .toBe(' classNameTrue');
+  });
+
+  test('test with only mods and undefined params', () => {
+    expect(classNames(undefined, { classNameTrue: true, classNameUndefined: undefined }, ['']))
+      .toBe(' classNameTrue');
+  });
+});
