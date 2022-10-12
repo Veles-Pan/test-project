04be39bb4165d9ca40866eb100eecda08b9ae0b9
Veles-Pan <barbic-vika@ya.ru>
2022-10-12 16:22:28 +0100

    feat: add ci

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,21 +8,27 @@ import { BuildOptions } from './types/config';
 export const buildPlugins = ({
   paths,
   isDevelopment,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new HtmlWebpackPlugin({
-    template: paths.htmlPath,
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new webpack.DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDevelopment),
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new ReactRefreshWebpackPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-  }),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: paths.htmlPath,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDevelopment),
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ];
+
+  if (isDevelopment) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
+  }
+  return plugins;
+};
